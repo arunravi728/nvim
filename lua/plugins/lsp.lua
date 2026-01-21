d58879c91a36c917a18e2319375ac84ee5d17362
Arun Ravi <arunravi728@gmail.com>
2026-01-20 22:08:10 -0800

    Use ;; to exit insert mode

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -201,9 +201,8 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        clangd = {},
+        pyright = {},
         rust_analyzer = {},
         intelephense = {},
 
