cd38053c044664d4bf6efbc63df31e59195f79ea
Arun Ravi <arunravi728@gmail.com>
2026-05-02 09:58:48 -0700

    Initial nvim config with telescope - inspired by kickstarter

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- Set <space> as the leader key
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Enable NerdFonts
+vim.g.have_nerd_font = true
+
+require 'options'
+require 'keymaps'
+require 'lazy-install'
+require 'plugins'
---
+++ lua/plugins.lua
@@ -0,0 +1,39 @@
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+require('lazy').setup({
+  -- NOTE: Plugins can be added via a link or github org/name. To run setup automatically, use `opts = {}`
+  { 'NMAC427/guess-indent.nvim', opts = {} },
+
+  require 'plugins/autopairs',
+  require 'plugins/telescope',
+
+  -- modular approach: using `require 'path.name'` will
+  -- include a plugin definition from file lua/path/name.lua
+}, { ---@diagnostic disable-line: missing-fields
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
