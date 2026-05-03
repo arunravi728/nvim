2bc60ff1f9e3100e13a2914ad121eabf93afacfe
Arun Ravi <arunravi728@gmail.com>
2026-05-03 11:40:40 -0700

    Updated nvim background color

--- lua/plugins/one-dark.lua
+++ lua/plugins/one-dark.lua
@@ -4,10 +4,16 @@ return {
 	config = function()
 		require("onedark").setup({
 			style = "darker",
-
+			colors = {
+				bg0 = "#000000",
+			},
+			highlights = {
+				Normal = { bg = "$bg0" },
+				NormalNC = { bg = "$bg0" },
+			},
 			code_style = {
 				comments = "none",
-				keywords = "bold",
+				keywords = "none",
 				functions = "none",
 				strings = "none",
 				variables = "none",
