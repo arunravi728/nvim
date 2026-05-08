b129c5064241d5aafc19f69b0464a6401f7005d3
Arun Ravi <arunravi728@gmail.com>
2026-05-07 21:27:22 -0700

    Remap find in current buffer key map

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -107,12 +107,12 @@ return {
 				end,
 			})
 
-			vim.keymap.set("n", "<leader>/", function()
+			vim.keymap.set("n", "<leader>ff", function()
 				builtin.live_grep({
 					grep_open_files = true,
 					prompt_title = "Live Grep in Open Files",
 				})
-			end, { desc = "[F]ind [/] in Open Files" })
+			end, { desc = "[F]ind in Open [F]iles" })
 
 			-- Shortcut for searching your Neovim configuration files
 			vim.keymap.set("n", "<leader>fn", function()
